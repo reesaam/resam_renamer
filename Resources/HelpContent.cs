11a2ec3f8e9e7d9ca69271fe7042813a05ef4837
Resam Taghipour <resam.t@gmail.com>
2026-02-01 13:51:18 +0330

    Reformat and Refactor

--- Resources/HelpContent.cs
+++ Resources/HelpContent.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ResamRenamer.Resources
+﻿namespace ResamRenamer.Resources
 {
     public class HelpContent
     {
-        public readonly string[] FirstContext = ["Resam Renamer", "This is a MultiPurpose Application for Organize, Rename, Ease and Create Material for Your Movies and Series Collection."
-        ];
-        public string[] RenameSection = ["Rename Section", "Rename Section."];
+        public readonly string[] FirstContext = ["Resam Renamer", "This is a MultiPurpose Application for Organize, Rename, Ease and Create Material for Your Movies and Series Collection."];
     }
 }
